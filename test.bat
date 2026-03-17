486d225db397ae66270ef4b03c170e0456bb68bb
Jared De Blander <jared0x90@gmail.com>
2026-03-17 03:03:57 -0400

    windows tests

--- test.bat
+++ test.bat
@@ -1,4 +1,8 @@
+@echo off
+echo Building...
 cargo build -q -p svsim-cli --release
+echo Testing...
 .\target\release\svsim.exe --json-test-dir parts/basic > report-parts-basic.json
 .\target\release\svsim.exe --json-test-dir parts/overture > report-parts-overture.json
+.\target\release\svsim.exe --json-test-dir parts/rv32i > report-parts-rv32i.json
 .\target\release\svsim.exe --json-test-dir parts/testing > report-parts-testing.json
