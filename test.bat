9f627b566f7bc39574a284de0b2813c71757a111
Jared De Blander <jared0x90@gmail.com>
2026-03-19 19:57:11 -0400

    12400f testing

--- test.bat
+++ test.bat
@@ -6,4 +6,6 @@ echo Testing...
 .\target\release\svsim.exe --json-test-dir parts/overture > docs/tests/report-parts-overture.json
 .\target\release\svsim.exe --json-test-dir parts/picorv32 > docs/tests/report-parts-picorv32.json
 .\target\release\svsim.exe --json-test-dir parts/rv32i > docs/tests/report-parts-rv32i.json
+.\target\release\svsim.exe --json-test-dir parts/sap1 > docs/tests/report-parts-sap1.json
 .\target\release\svsim.exe --json-test-dir parts/testing > docs/tests/report-parts-testing.json
+echo Done
