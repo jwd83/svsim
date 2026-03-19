26dc5a9c4e407cc2126d1a760975a211006faa33
Jared De Blander <jared0x90@gmail.com>
2026-03-19 01:55:02 -0400

    cleanup

--- ref/test.bat
+++
@@ -1,3 +0,0 @@
-uv run pysvsim.py parts/testing/ > results/parts_testing.txt
-uv run pysvsim.py parts/basic/ > results/parts_basic.txt
-uv run pysvsim.py parts/overture/ > results/parts_overture.txt
