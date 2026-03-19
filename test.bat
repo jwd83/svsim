7b9e246721dd8a5b38abaf7a1a84e5dfa55d4f5a
Jared De Blander <jared0x90@gmail.com>
2026-03-19 18:53:30 -0400

    reporting updates

--- test.bat
+++ test.bat
@@ -2,8 +2,8 @@
 echo Building...
 cargo build -q -p svsim-cli --release
 echo Testing...
-.\target\release\svsim.exe --json-test-dir parts/basic > report-parts-basic.json
-.\target\release\svsim.exe --json-test-dir parts/overture > report-parts-overture.json
-.\target\release\svsim.exe --json-test-dir parts/picorv32 > report-parts-picorv32.json
-.\target\release\svsim.exe --json-test-dir parts/rv32i > report-parts-rv32i.json
-.\target\release\svsim.exe --json-test-dir parts/testing > report-parts-testing.json
+.\target\release\svsim.exe --json-test-dir parts/basic > docs/tests/report-parts-basic.json
+.\target\release\svsim.exe --json-test-dir parts/overture > docs/tests/report-parts-overture.json
+.\target\release\svsim.exe --json-test-dir parts/picorv32 > docs/tests/report-parts-picorv32.json
+.\target\release\svsim.exe --json-test-dir parts/rv32i > docs/tests/report-parts-rv32i.json
+.\target\release\svsim.exe --json-test-dir parts/testing > docs/tests/report-parts-testing.json
--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 echo "Building..."
 cargo build -q -p svsim-cli --release
 echo "Testing..."
-./target/release/svsim --json-test-dir parts/basic > report-parts-basic.json
-./target/release/svsim --json-test-dir parts/overture > report-parts-overture.json
-./target/release/svsim --json-test-dir parts/picorv32 > report-parts-picorv32.json
-./target/release/svsim --json-test-dir parts/rv32i > report-parts-rv32i.json
-./target/release/svsim --json-test-dir parts/sap1 > report-parts-sap1.json
-./target/release/svsim --json-test-dir parts/testing > report-parts-testing.json
+./target/release/svsim --json-test-dir parts/basic > docs/tests/report-parts-basic.json
+./target/release/svsim --json-test-dir parts/overture > docs/tests/report-parts-overture.json
+./target/release/svsim --json-test-dir parts/picorv32 > docs/tests/report-parts-picorv32.json
+./target/release/svsim --json-test-dir parts/rv32i > docs/tests/report-parts-rv32i.json
+./target/release/svsim --json-test-dir parts/sap1 > docs/tests/report-parts-sap1.json
+./target/release/svsim --json-test-dir parts/testing > docs/tests/report-parts-testing.json
 echo "Done"
